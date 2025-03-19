cac620ab170a2ad0cea1f367ab98502280482c1b
Egor <egorrogacev030@gmail.com>
2025-03-19 17:20:09 +0300

    added weekly block

---
+++ src/widgets/Weekly/Weekly.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export const Weekly = () => {
+    return (
+        <div className={styles.weekly}>
+            <h2 className={styles.title}>Weekly - Top NFT</h2>
+        </div>
+    )
+}
---
+++ src/widgets/Weekly/WeeklyCard/WeeklyCard.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export const WeeklyCard = () => {
+    return (
+        <div className={styles.weeklyCard}>
+
+        </div>
+    )
+}
